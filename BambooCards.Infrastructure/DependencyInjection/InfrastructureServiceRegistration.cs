b237085ed530337309950da08f8aa9f8359ee223
Kushal test <kushal.test1998@hotmail.com>
2026-02-28 11:56:54 +0530

    validate issuer false

--- BambooCards.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
+++ BambooCards.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -66,7 +66,7 @@ namespace BambooCards.Infrastructure.DependencyInjection
 
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuer = true,
+                        ValidateIssuer = false,
                         ValidIssuer = keycloakUri + keycloakOptions.Authority,
                         ValidateAudience = true,
                         ValidAudience = keycloakOptions.Audience,
